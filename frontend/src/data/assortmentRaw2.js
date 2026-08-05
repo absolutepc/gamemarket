8501911d51f874912dc27962be83ea71989a3930
absolutepc <u.dakaev2016@gmail.com>
2026-08-05 08:33:17 +0300

    fix: add full catalog data files (658 items)

---
+++ frontend/src/data/assortmentRaw2.js
@@ -0,0 +1,9 @@
+export const CATALOG_RAW_2 = `Netflix|Netflix|netflix|a
+Twitch|Twitch|twitch|a
+DeepSeek|DeepSeek|deepseek|a
+Midjourney|Midjourney|midjourney|a
+Epic Games|Epic Games|epic-games|a
+Canva|Canva|canva|a
+GeForce NOW|GeForce NOW|geforce-now|a
+EA Play|EA Play|ea-play|a
+`;
