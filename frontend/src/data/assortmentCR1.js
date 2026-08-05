0a028df3ab0549a0dd3d4523e503362126ee5798
absolutepc <u.dakaev2016@gmail.com>
2026-08-05 08:41:30 +0300

    revert: restore catalog CR1-CR2 (mobile games)

---
+++ frontend/src/data/assortmentCR1.js
@@ -0,0 +1,112 @@
+export const CATALOG_RAW_1 = `Yappy|Yappy|yappy|a
+Trovo|Trovo|trovo|a
+Crosshair X|Crosshair X|crosshair-x|a
+Аудиоредакторы|Аудиоредакторы|fl-studio|a
+Slack|Slack|slack|a
+Hailuo|Hailuo|hailuo|a
+Luma|Luma AI|luma|a
+Windsurf|Windsurf|windsurf|a
+YoloMouse - Cursor Changer|YoloMouse|yolomouse-cursor-changer|a
+Quillbot|Quillbot|quillbot|a
+ArtList|Artlist|artlist|a
+OpenArt|OpenArt|openart|a
+Chutes AI|Chutes|chutes-ai|a
+Mimo|Mimo|mimo|a
+Tango Live|Tango Live|tango-live|a
+Ideogram|Ideogram|ideogram|a
+Qobuz|Qobuz|qobuz|a
+n8n|n8n|n8n|a
+Ahrefs|Ahrefs|ahrefs|a
+Hugging Face|Hugging Face|hugging-face|a
+GOG|GOG|gog|a
+NoPing|NoPing|noping|a
+Arena Breakout|Arena Breakout|arena-breakout|m
+PUBG Mobile|PUBG Mobile|pubg-mobile|m
+Brawl Stars|Brawl Stars|brawl-stars|m
+Clash of Clans|Clash of Clans|clash-of-clans|m
+Standoff 2|Standoff 2|standoff-2|m
+Clash Royale|Clash Royale|clash-royale|m
+EA SPORTS FC Mobile|EA SPORTS FC Mobile FC Mobile|fc-mobile|m
+Black Russia|Black Russia|black-russia|m
+Mobile Legends|Mobile Legends|mobile-legends|m
+Free Fire|Free Fire|free-fire|m
+CoD Mobile|Call of Duty Mobile|cod-mobile|m
+Mortal Kombat|Mortal Kombat Mobile|mortal-kombat|m
+Drag Racing|Drag Racing|drag-racing|m
+Blockman GO|Blockman GO|blockman-go|m
+Last Island|Last Island of Survival|last-island|m
+eFootball|eFootball|efootball|m
+Diablo Immortal|Diablo Immortal|diablo-immortal|m
+Матрешка RP|Матрешка|matreshka-rp|m
+Forge of Empires|Forge of Empires|forge-of-empires|m
+Oxide|Oxide|oxide|m
+Grand Mobile|Grand Mobile|grand-mobile|m
+Avakin Life|Avakin Life|avakin-life|m
+Bullet Echo|Bullet Echo|bullet-echo|m
+Car Parking|Car Parking Multiplayer|car-parking|m
+Car Parking 2|Car Parking Multiplayer 2|car-parking-2|m
+My Singing Monsters|My Singing Monsters|my-singing-monsters|m
+Super Sus|Super Sus|super-sus|m
+Rush Royale|Rush Royale|rush-royale|m
+Rise of Kingdoms|Rise of Kingdoms|rise-of-kingdoms|m
+Cookie Run|Cookie Run|cookie-run|m
+Whiteout Survival|Whiteout Survival|whiteout-survival|m
+Township|Township|township|m
+MadOut 2|MadOut 2|madout-2|m
+Last Day on Earth|Last Day on Earth|last-day-on-earth|m
+Beatstar|Beatstar|beatstar|m
+CarX Street|CarX Street|carx-street|m
+Wild Rift|Wild Rift|wild-rift|m
+ZEPETO|ZEPETO|zepeto|m
+Дурак Онлайн|Дурак|durak|m
+The Spike|The Spike|the-spike|m
+WildCraft|WildCraft|wildcraft|m
+Zooba|Zooba|zooba|m
+Crossout Mobile|Crossout Mobile|crossout-mobile|m
+Viking Rise|Viking Rise|viking-rise|m
+Soul Knight|Soul Knight|soul-knight|m
+Block Strike|Block Strike|block-strike|m
+Nulls Brawl|Nulls Brawl|nulls-brawl|m
+Hay Day|Hay Day|hay-day|m
+CATS|CATS Crash Arena|cats|m
+CarX Drift|CarX Drift|carx-drift|m
+Counter Attack|Counter Attack|counter-attack|m
+Shadow Fight 3|Shadow Fight 3|shadow-fight-3|m
+Highrise|Highrise|highrise|m
+Pixel Gun 3D|Pixel Gun 3D|pixel-gun-3d|m
+Gacha Club|Gacha Club|gacha-club|m
+Hill Climb 2|Hill Climb Racing|hill-climb-2|m
+Royal Match|Royal Match|royal-match|m
+PUBG New State|PUBG New State|pubg-new-state|m
+Roblox|Roblox|roblox|m
+Genshin|Genshin|genshin|m
+Honkai Star Rail|Honkai Star Rail|honkai-star-rail|m
+Zenless Zone Zero|Zenless Zone Zero|zenless-zone-zero|m
+Identity V|Identity V|identity-v|m
+Blood Strike|Blood Strike|blood-strike|m
+Delta Force|Delta Force|delta-force|m
+Honkai Impact 3rd|Honkai Impact|honkai-impact-3rd|m
+Tower of Fantasy|Tower of Fantasy|tower-of-fantasy|m
+Wuthering Waves|Wuthering Waves|wuthering-waves|m
+AFK Arena|AFK Arena|afk-arena|m
+Summoners War|Summoners War|summoners-war|m
+Raid Shadow Legends|RAID Shadow Legends|raid-shadow-legends|m
+Marvel Snap|Marvel Snap|marvel-snap|m
+Marvel Contest|Marvel Contest of Champions|marvel-contest|m
+Pokémon GO|Pokemon GO|pok-mon-go|m
+Pokémon Unite|Pokemon Unite|pok-mon-unite|m
+Monster Hunter Now|Monster Hunter Now|monster-hunter-now|m
+Call of Duty Warzone Mobile|Warzone Mobile|call-of-duty-warzone-mobile|m
+Apex Legends Mobile|Apex Legends Mobile|apex-legends-mobile|m
+Farlight 84|Farlight 84|farlight-84|m
+Once Human Mobile|Once Human Mobile|once-human-mobile|m
+State of Survival|State of Survival|state-of-survival|m
+Guns of Glory|Guns of Glory|guns-of-glory|m
+Evony|Evony|evony|m
+Lords Mobile|Lords Mobile|lords-mobile|m
+King of Avalon|King of Avalon|king-of-avalon|m
+Rise of Empires|Rise of Empires|rise-of-empires|m
+Age of Empires Mobile|Age of Empires Mobile|age-of-empires-mobile|m
+Infinity Kingdom|Infinity Kingdom|infinity-kingdom|m
+Dragon City|Dragon City|dragon-city|m
+Monster Legends|Monster Legends|monster-legends|m`;
