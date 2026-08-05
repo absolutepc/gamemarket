b78b7b7ce88f0fcf759b27abc08cd6349dd1632a
absolutepc <u.dakaev2016@gmail.com>
2026-08-05 08:42:17 +0300

    revert: restore catalog CR3 (PC games)

---
+++ frontend/src/data/assortmentCR3.js
@@ -0,0 +1,112 @@
+export const CATALOG_RAW_3 = `PUBG|PUBG|pubg|p
+Valorant|Valorant|valorant|p
+Escape From Tarkov|Tarkov|escape-from-tarkov|p
+CS2|Counter-Strike|cs2|p
+Fortnite|Fortnite|fortnite|p
+Minecraft|Minecraft|minecraft|p
+Dota 2|Dota 2|dota-2|p
+GTA 5|GTA|gta-5|p
+Warface|Warface|warface|p
+Rust|Rust|rust|p
+Apex Legends|Apex Legends|apex-legends|p
+League of Legends|League of Legends|league-of-legends|p
+Overwatch 2|Overwatch|overwatch-2|p
+World of Tanks|World of Tanks|world-of-tanks|p
+Dead by Daylight|Dead by Daylight|dead-by-daylight|p
+Team Fortress 2|Team Fortress|tf2|p
+Rainbow Six Siege|Rainbow Six|rainbow-six|p
+GTA Online|GTA Online|gta-online|p
+Lost Ark|Lost Ark|lost-ark|p
+Path of Exile|Path of Exile|path-of-exile|p
+Elden Ring|Elden Ring|elden-ring|p
+RDR 2|Red Dead|red-dead-redemption-2|p
+Cyberpunk 2077|Cyberpunk|cyberpunk-2077|p
+Destiny 2|Destiny 2|destiny-2|p
+Rocket League|Rocket League|rocket-league|p
+Sea of Thieves|Sea of Thieves|sea-of-thieves|p
+Hunt Showdown|Hunt Showdown|hunt-showdown|p
+Albion Online|Albion|albion-online|p
+New World|New World|new-world|p
+Once Human|Once Human|once-human|p
+Palworld|Palworld|palworld|p
+Helldivers 2|Helldivers 2|helldivers-2|p
+Counter-Strike 2|CS2|counter-strike-2|p
+Teamfight Tactics|TFT|teamfight-tactics|p
+Legends of Runeterra|Runeterra|legends-of-runeterra|p
+Warzone|Warzone|warzone|p
+Black Ops 6|Black Ops 6|black-ops-6|p
+Modern Warfare 3|MW3|modern-warfare-3|p
+FIFA 23|FIFA 23|fifa-23|p
+EA FC 24|EA FC 24|ea-fc-24|p
+EA FC 25|EA FC 25|ea-fc-25|p
+NBA 2K24|NBA 2K24|nba-2k24|p
+NBA 2K25|NBA 2K25|nba-2k25|p
+Madden 25|Madden 25|madden-25|p
+Forza Horizon 5|Forza Horizon 5|forza-horizon-5|p
+Forza Motorsport|Forza Motorsport|forza-motorsport|p
+Gran Turismo 7|Gran Turismo|gran-turismo-7|p
+Assetto Corsa|Assetto Corsa|assetto-corsa|p
+iRacing|iRacing|iracing|p
+BeamNG.drive|BeamNG|beamng-drive|p
+Euro Truck Simulator 2|ETS2|euro-truck-simulator-2|p
+American Truck Simulator|ATS|american-truck-simulator|p
+Farming Simulator 25|Farming Simulator|farming-simulator-25|p
+Microsoft Flight Simulator|Flight Simulator|microsoft-flight-simulator|p
+Cities Skylines 2|Cities Skylines|cities-skylines-2|p
+The Sims 4|The Sims 4|the-sims-4|p
+SimCity|SimCity|simcity|p
+Civilization VI|Civilization|civilization-vi|p
+Civilization VII|Civ 7|civilization-vii|p
+Total War Warhammer 3|Warhammer 3|total-war-warhammer-3|p
+Age of Empires IV|Age of Empires|age-of-empires-iv|p
+Company of Heroes 3|Company of Heroes|company-of-heroes-3|p
+Hearts of Iron IV|HOI4|hearts-of-iron-iv|p
+Europa Universalis IV|EU4|europa-universalis-iv|p
+Crusader Kings III|CK3|crusader-kings-iii|p
+Stellaris|Stellaris|stellaris|p
+Victoria 3|Victoria 3|victoria-3|p
+Baldurs Gate 3|Baldurs Gate 3|baldur-s-gate-3|p
+Divinity Original Sin 2|Divinity|divinity-original-sin-2|p
+Pathfinder Wrath|Pathfinder|pathfinder-wrath|p
+Diablo IV|Diablo 4|diablo-iv|p
+Diablo III|Diablo 3|diablo-iii|p
+Path of Exile 2|PoE 2|path-of-exile-2|p
+Last Epoch|Last Epoch|last-epoch|p
+Torchlight Infinite|Torchlight|torchlight-infinite|p
+Wolcen|Wolcen|wolcen|p
+Grim Dawn|Grim Dawn|grim-dawn|p
+Hades|Hades|hades|p
+Hades II|Hades 2|hades-ii|p
+Dead Cells|Dead Cells|dead-cells|p
+Hollow Knight|Hollow Knight|hollow-knight|p
+Silksong|Silksong|silksong|p
+Celeste|Celeste|celeste|p
+Stardew Valley|Stardew Valley|stardew-valley|p
+Terraria|Terraria|terraria|p
+Valheim|Valheim|valheim|p
+Grounded|Grounded|grounded|p
+Enshrouded|Enshrouded|enshrouded|p
+V Rising|V Rising|v-rising|p
+Sons of the Forest|Sons of the Forest|sons-of-the-forest|p
+The Forest|The Forest|the-forest|p
+Green Hell|Green Hell|green-hell|p
+Subnautica|Subnautica|subnautica|p
+Subnautica Below Zero|Below Zero|subnautica-below-zero|p
+No Mans Sky|No Mans Sky|no-man-s-sky|p
+Astroneer|Astroneer|astroneer|p
+Satisfactory|Satisfactory|satisfactory|p
+Factorio|Factorio|factorio|p
+Dyson Sphere Program|Dyson Sphere|dyson-sphere-program|p
+RimWorld|RimWorld|rimworld|p
+Oxygen Not Included|ONI|oxygen-not-included|p
+Dont Starve Together|DST|don-t-starve-together|p
+Project Zomboid|Project Zomboid|project-zomboid|p
+7 Days to Die|7 Days to Die|7-days-to-die|p
+DayZ|DayZ|dayz|p
+SCUM|SCUM|scum|p
+Unturned|Unturned|unturned|p
+Left 4 Dead 2|L4D2|left-4-dead-2|p
+Back 4 Blood|Back 4 Blood|back-4-blood|p
+Deep Rock Galactic|Deep Rock|deep-rock-galactic|p
+Vermintide 2|Vermintide|vermintide-2|p
+Darktide|Darktide|darktide|p`;
