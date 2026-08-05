79713ee01207059fc471db9d80f809111e74c51d
absolutepc <u.dakaev2016@gmail.com>
2026-08-05 08:41:54 +0300

    revert: restore catalog CR2

---
+++ frontend/src/data/assortmentCR2.js
@@ -0,0 +1,112 @@
+export const CATALOG_RAW_2 = `Merge Dragons|Merge Dragons|merge-dragons|m
+Homescapes|Homescapes|homescapes|m
+Gardenscapes|Gardenscapes|gardenscapes|m
+Fishdom|Fishdom|fishdom|m
+Toy Blast|Toy Blast|toy-blast|m
+Toon Blast|Toon Blast|toon-blast|m
+Candy Crush|Candy Crush|candy-crush|m
+Candy Crush Soda|Candy Crush Soda|candy-crush-soda|m
+Coin Master|Coin Master|coin-master|m
+Monopoly GO|Monopoly GO|monopoly-go|m
+Board Kings|Board Kings|board-kings|m
+Trivia Crack|Trivia Crack|trivia-crack|m
+8 Ball Pool|8 Ball Pool|8-ball-pool|m
+Billiards City|Billiards|billiards-city|m
+Dream League Soccer|Dream League|dream-league-soccer|m
+FIFA Mobile|FIFA Mobile|fifa-mobile|m
+NBA 2K Mobile|NBA 2K Mobile|nba-2k-mobile|m
+Asphalt 9|Asphalt 9|asphalt-9|m
+Asphalt 8|Asphalt 8|asphalt-8|m
+Real Racing 3|Real Racing 3|real-racing-3|m
+CSR Racing 2|CSR 2|csr-racing-2|m
+Need for Speed No Limits|NFS No Limits|need-for-speed-no-limits|m
+Subway Surfers|Subway Surfers|subway-surfers|m
+Temple Run 2|Temple Run|temple-run-2|m
+Geometry Dash|Geometry Dash|geometry-dash|m
+Among Us|Among Us|among-us|m
+Fall Guys|Fall Guys|fall-guys|m
+Stumble Guys|Stumble Guys|stumble-guys|m
+Party Animals|Party Animals|party-animals|m
+Brawlhalla|Brawlhalla|brawlhalla|m
+Shadow Fight 2|Shadow Fight 2|shadow-fight-2|m
+Shadow Fight Arena|Shadow Fight Arena|shadow-fight-arena|m
+Injustice 2|Injustice 2|injustice-2|m
+Tekken|Tekken|tekken|m
+Street Fighter DUEL|Street Fighter|street-fighter-duel|m
+Dragon Ball Legends|Dragon Ball Legends|dragon-ball-legends|m
+One Piece Bounty|One Piece Bounty Rush|one-piece-bounty|m
+Naruto Ultimate Ninja|Naruto|naruto-ultimate-ninja|m
+Bleach Brave Souls|Bleach|bleach-brave-souls|m
+Fate Grand Order|Fate/Grand Order|fate-grand-order|m
+Azur Lane|Azur Lane|azur-lane|m
+Girls Frontline|Girls Frontline|girls-frontline|m
+Nikke|Goddess of Victory Nikke|nikke|m
+Blue Archive|Blue Archive|blue-archive|m
+Arknights|Arknights|arknights|m
+Punishing Gray Raven|Punishing Gray Raven|punishing-gray-raven|m
+Reverse 1999|Reverse 1999|reverse-1999|m
+Limbus Company|Limbus Company|limbus-company|m
+Uma Musume|Uma Musume|uma-musume|m
+Princess Connect|Princess Connect|princess-connect|m
+Epic Seven|Epic Seven|epic-seven|m
+Idle Heroes|Idle Heroes|idle-heroes|m
+AFK Journey|AFK Journey|afk-journey|m
+Watcher of Realms|Watcher of Realms|watcher-of-realms|m
+Hero Wars|Hero Wars|hero-wars|m
+Raid Land|Raid Land|raid-land|m
+Last War|Last War Survival|last-war|m
+Doomsday Last Survivors|Doomsday|doomsday-last-survivors|m
+Puzzles and Survival|Puzzles & Survival|puzzles-and-survival|m
+Frost & Flame|Frost and Flame|frost-flame|m
+Project Makeover|Project Makeover|project-makeover|m
+Episode|Episode|episode|m
+Choices|Choices|choices|m
+Love and Deepspace|Love and Deepspace|love-and-deepspace|m
+Tears of Themis|Tears of Themis|tears-of-themis|m
+MrLove|Mr Love Queen Choice|mrlove|m
+Dead by Daylight Mobile|DbD Mobile|dead-by-daylight-mobile|m
+Five Nights at Freddy|FNAF|five-nights-at-freddy-s|m
+Granny|Granny|granny|m
+Horror Tale|Horror|horror-tale|m
+SimCity BuildIt|SimCity BuildIt|simcity-buildit|m
+The Sims Mobile|The Sims Mobile|the-sims-mobile|m
+Animal Crossing Pocket|Animal Crossing|animal-crossing-pocket|m
+Stardew Valley Mobile|Stardew Valley Mobile|stardew-valley-mobile|m
+Terraria Mobile|Terraria Mobile|terraria-mobile|m
+Dont Starve|Dont Starve|don-t-starve|m
+Altos Odyssey|Alto|alto-s-odyssey|m
+Monument Valley|Monument Valley|monument-valley|m
+Genshin Impact|Genshin Impact|genshin-impact|m
+Delta Force Hawk Ops|Delta Force Mobile|delta-force-hawk-ops|m
+War Thunder Mobile|War Thunder Mobile|war-thunder-mobile|m
+World of Tanks Blitz|World of Tanks Blitz|world-of-tanks-blitz|m
+World of Warships Blitz|Warships Blitz|world-of-warships-blitz|m
+Standoff Critical Strike|Critical Strike|standoff-critical-strike|m
+Critical Ops|Critical Ops|critical-ops|m
+Modern Strike Online|Modern Strike|modern-strike-online|m
+Bullet Force|Bullet Force|bullet-force|m
+Guns of Boom|Guns of Boom|guns-of-boom|m
+Sniper 3D|Sniper 3D|sniper-3d|m
+Deer Hunter|Deer Hunter|deer-hunter|m
+Golf Clash|Golf Clash|golf-clash|m
+Tennis Clash|Tennis Clash|tennis-clash|m
+Basketball Stars|Basketball Stars|basketball-stars|m
+Soccer Stars|Soccer Stars|soccer-stars|m
+Score Hero|Score Hero|score-hero|m
+Top Eleven|Top Eleven|top-eleven|m
+PES Club Manager|eFootball PES|pes-club-manager|m
+Madden NFL|Madden|madden-nfl|m
+MLB Tap Sports|MLB|mlb-tap-sports|m
+Wrestling Empire|Wrestling|wrestling-empire|m
+Talking Tom|Talking Tom|talking-tom|m
+My Talking Angela|Talking Angela|my-talking-angela|m
+Toca Life|Toca Life|toca-life|m
+Minecraft PE|Minecraft PE|minecraft-pe|m
+Block Craft 3D|Block Craft|block-craft-3d|m
+The Sandbox|The Sandbox|the-sandbox|m
+Decentraland|Decentraland|decentraland|m
+Cats Gang|Cats|cats-gang|m
+X Empire|X Empire|x-empire|m
+Major|Major|major|m
+City Holder|City Holder|city-holder|m
+Soft Hamster|Hamster|soft-hamster|m`;
