63e0c4e4a005cdfbf5154b38f21f7bfe1a7ce073
absolutepc <u.dakaev2016@gmail.com>
2026-08-05 08:42:43 +0300

    revert: restore catalog CR4

---
+++ frontend/src/data/assortmentCR4.js
@@ -0,0 +1,112 @@
+export const CATALOG_RAW_4 = `Space Marine 2|Space Marine 2|space-marine-2|p
+Warhammer 40K|Warhammer 40K|warhammer-40k|p
+Phasmophobia|Phasmophobia|phasmophobia|p
+Lethal Company|Lethal Company|lethal-company|p
+Content Warning|Content Warning|content-warning|p
+PEAK|PEAK|peak|p
+R.E.P.O.|REPO|r-e-p-o|p
+Schedule I|Schedule I|schedule-i|p
+Escape the Backrooms|Backrooms|escape-the-backrooms|p
+Gang Beasts|Gang Beasts|gang-beasts|p
+Human Fall Flat|Human Fall Flat|human-fall-flat|p
+It Takes Two|It Takes Two|it-takes-two|p
+Split Fiction|Split Fiction|split-fiction|p
+Overcooked 2|Overcooked|overcooked-2|p
+Moving Out|Moving Out|moving-out|p
+PlateUp|PlateUp|plateup|p
+Multiversus|Multiversus|multiversus|p
+Smash Bros|Smash Bros|smash-bros|p
+Tekken 8|Tekken 8|tekken-8|p
+Street Fighter 6|Street Fighter 6|street-fighter-6|p
+Mortal Kombat 1|MK1|mortal-kombat-1|p
+Guilty Gear Strive|Guilty Gear|guilty-gear-strive|p
+Nickelodeon All-Star Brawl|Nick Brawl|nickelodeon-all-star-brawl|p
+osu!|osu|osu|p
+Beat Saber|Beat Saber|beat-saber|p
+VRChat|VRChat|vrchat|p
+Rec Room|Rec Room|rec-room|p
+ChilloutVR|ChilloutVR|chilloutvr|p
+Neos VR|Neos|neos-vr|p
+Desktop Mate|Desktop Mate|desktop-mate|p
+Lively Wallpaper|Lively|lively-wallpaper|p
+Rainmeter|Rainmeter|rainmeter|p
+ESEA|ESEA|esea|p
+GamersClub|GamersClub|gamersclub|p
+War Thunder|War Thunder|war-thunder|p
+World of Warships|World of Warships|world-of-warships|p
+World of Warplanes|World of Warplanes|world-of-warplanes|p
+Crossout|Crossout|crossout|p
+Armored Warfare|Armored Warfare|armored-warfare|p
+Enlisted|Enlisted|enlisted|p
+Hell Let Loose|Hell Let Loose|hell-let-loose|p
+Squad|Squad|squad|p
+Arma 3|Arma 3|arma-3|p
+Arma Reforger|Arma Reforger|arma-reforger|p
+Ready or Not|Ready or Not|ready-or-not|p
+Door Kickers|Door Kickers|door-kickers|p
+Insurgency Sandstorm|Insurgency|insurgency-sandstorm|p
+Ground Branch|Ground Branch|ground-branch|p
+Escape from Tarkov Arena|Tarkov Arena|escape-from-tarkov-arena|p
+Gray Zone Warfare|Gray Zone|gray-zone-warfare|p
+Marauders|Marauders|marauders|p
+Cycle Frontier|Cycle Frontier|cycle-frontier|p
+Dark and Darker|Dark and Darker|dark-and-darker|p
+Dungeonborne|Dungeonborne|dungeonborne|p
+Wo Long|Wo Long|wo-long|p
+Nioh 2|Nioh 2|nioh-2|p
+Lies of P|Lies of P|lies-of-p|p
+Sekiro|Sekiro|sekiro|p
+Bloodborne|Bloodborne|bloodborne|p
+Demons Souls|Demons Souls|demon-s-souls|p
+Dark Souls 3|Dark Souls 3|dark-souls-3|p
+Dark Souls Remastered|Dark Souls|dark-souls-remastered|p
+Elden Ring Nightreign|Nightreign|elden-ring-nightreign|p
+Black Myth Wukong|Black Myth Wukong|black-myth-wukong|p
+Ghost of Tsushima|Ghost of Tsushima|ghost-of-tsushima|p
+Horizon Forbidden West|Horizon|horizon-forbidden-west|p
+God of War|God of War|god-of-war|p
+God of War Ragnarok|Ragnarok|god-of-war-ragnarok|p
+Spider-Man Remastered|Spider-Man|spider-man-remastered|p
+Spider-Man 2|Spider-Man 2|spider-man-2|p
+The Last of Us Part I|The Last of Us|the-last-of-us-part-i|p
+The Last of Us Part II|TLOU2|the-last-of-us-part-ii|p
+Uncharted Legacy|Uncharted|uncharted-legacy|p
+Death Stranding|Death Stranding|death-stranding|p
+Death Stranding 2|Death Stranding 2|death-stranding-2|p
+Metal Gear Solid|Metal Gear|metal-gear-solid|p
+Resident Evil 4|RE4|resident-evil-4|p
+Resident Evil Village|RE Village|resident-evil-village|p
+Resident Evil 2|RE2|resident-evil-2|p
+Silent Hill 2|Silent Hill 2|silent-hill-2|p
+Alan Wake 2|Alan Wake 2|alan-wake-2|p
+Control|Control|control|p
+Quantum Break|Quantum Break|quantum-break|p
+Max Payne 3|Max Payne|max-payne-3|p
+Red Dead Online|RDO|red-dead-online|p
+GTA Trilogy|GTA Trilogy|gta-trilogy|p
+GTA 4|GTA IV|gta-4|p
+Saints Row|Saints Row|saints-row|p
+Watch Dogs 2|Watch Dogs|watch-dogs-2|p
+Sleeping Dogs|Sleeping Dogs|sleeping-dogs|p
+Mafia Definitive|Mafia|mafia-definitive|p
+Mafia 3|Mafia III|mafia-3|p
+Yakuza Like a Dragon|Yakuza|yakuza-like-a-dragon|p
+Like a Dragon Infinite Wealth|Infinite Wealth|like-a-dragon-infinite-wealth|p
+Persona 5 Royal|Persona 5|persona-5-royal|p
+Persona 3 Reload|Persona 3|persona-3-reload|p
+Final Fantasy XIV|FFXIV|final-fantasy-xiv|p
+Final Fantasy XVI|FFXVI|final-fantasy-xvi|p
+Final Fantasy VII Remake|FF7 Remake|final-fantasy-vii-remake|p
+Dragon Quest XI|Dragon Quest|dragon-quest-xi|p
+Monster Hunter World|MHW|monster-hunter-world|p
+Monster Hunter Wilds|MH Wilds|monster-hunter-wilds|p
+Monster Hunter Rise|MH Rise|monster-hunter-rise|p
+Throne and Liberty|Throne and Liberty|throne-and-liberty|p
+Black Desert Online|BDO|black-desert-online|p
+Guild Wars 2|Guild Wars 2|guild-wars-2|p
+The Elder Scrolls Online|ESO|the-elder-scrolls-online|p
+Fallout 76|Fallout 76|fallout-76|p
+Fallout 4|Fallout 4|fallout-4|p
+Skyrim|Skyrim|skyrim|p
+Oblivion Remastered|Oblivion|oblivion-remastered|p
+Starfield|Starfield|starfield|p`;
